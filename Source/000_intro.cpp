15b615bed2cf1e441da239cf6894631c6a6a05c1
seojeon9 <underloose18@gmail.com>
2022-09-14 21:59:50 +0900

    [0914] 알고리즘 교안_2

---
+++ Source/000_intro.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+multiset<int> s;
+int main(){
+	s.insert(12);
+	s.insert(10);
+	s.insert(2);
+	s.insert(10);
+	s.insert(90);
+	s.insert(85);
+	s.insert(45);
+	
+	cout << "Multiset elements after sort\n";
+	for( auto it = s.)
+	return 0;	
+}
+
