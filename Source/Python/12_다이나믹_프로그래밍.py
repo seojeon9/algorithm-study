ebbb7673ff5280ca79d05b4acb6e66c0c1336794
seojeon9 <underloose18@gmail.com>
2022-11-25 21:36:47 +0900

    [1125] 다이나믹 프로그래밍

--- Source/Python/12_다이나믹_프로그래밍.py
+++ Source/Python/12_다이나믹_프로그래밍.py
@@ -119,6 +119,27 @@ print(d[n])
 
 # 5. 효율적인 화폐 구성
 
+# 정수 N,M을 입력받기
+n,m = map(int, input().split())
+# N개의 화폐 단위 정보를 입력받기
+array = []
+for i in range(n):
+    array.append(int(input()))
+    
+# 한 번 계산된 결과를 저ㅓ장하기 위한 DP 테이블 초기화
+d = [10001] * (m+1)
+
+# 다이나믹 프로그래밍 진행
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m+1):
+        if d[j -array[i]] != 10001:
+            d[j] = min(d[j -array[i]] + 1)
+            
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
 
 
 
