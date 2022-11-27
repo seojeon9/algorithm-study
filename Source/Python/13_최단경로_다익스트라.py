d6863ffbde938201f824d3ac863715a87927bd47
seojeon9 <underloose18@gmail.com>
2022-11-27 22:03:09 +0900

    [1127] 최단경로 다익스트라 간단버전

---
+++ Source/Python/13_최단경로_다익스트라.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 18:51:46 2022
+
+@author: tjwjd
+"""
+
+# 가장 빠른 길 찾기
+# 다익스트라 최간 경로 알고리즘
+# 여러 개의 노드가 있을 때, 특정한 노드에서 출바라여 다른 노드로 가는 각각의 최단 경로를 구해주는 알고리즘
+# 음의 간선이 없을 때 정상적으로 동작
+
+# 1. 출발 노드를 설정한다
+# 2. 최단 거리 테이블을 초기화한다.
+# 3. 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선책한다.
+# 4. 해당 노드를 거쳐 다른 노드로 가는 비용을 계산하여 최단 거리 테이블을 생신한다.
+# 5. 위 과정에서 3. 4. 번을 반복한다.
+
+# 간단한 다익스트라 알고리즘
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 도느에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n+1)]
+# 방문한 적이 있는지 체킇는 목적의 리스트를 만들기
+visited = [False] * (n+1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (N+1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 로드로 가는 비용이 c라는 의미
+    graph[a].append((b,c))
+    
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0 # 가장 최단거리가 짧은 노드
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n-1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+
+# 시간 복잡도O(V2)
+# 노드의 개수가 10,000개 넘어가는 경우 해결 x
+
+
+
+
+
+
